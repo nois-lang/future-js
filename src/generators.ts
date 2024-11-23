a1cfd972db4ce7257b6f74ecb482e5dace4a4086
ivanjermakov <ivanjermakov1@gmail.com>
2024-11-23 21:06:37 +0100

    Generators: task from closure and from promise

--- src/generators.ts
+++ src/generators.ts
@@ -1,13 +1,13 @@
-type Task<T> = (...any: any[]) => Generator<any, T>
+type Future<T> = (...any: any[]) => Generator<any, T>
 
-const never = (): Task<void> =>
+const never = (): Future<void> =>
     function* (): Generator<void> {
         while (true) {
             yield
         }
     }
 
-const delay = (ms: number): Task<number> =>
+const delay = (ms: number): Future<number> =>
     function* (): Generator<void, number> {
         const doneBy = performance.now() + ms
         while (doneBy >= performance.now()) {
@@ -16,28 +16,57 @@ const delay = (ms: number): Task<number> =>
         return 5
     }
 
-const longFive: Task<number> = function* () {
+const fromClosure = function* <T>(fn: (done: (res: T) => void) => void): Generator<any, T> {
+    let done = false
+    let result: T | undefined
+    fn(r => {
+        done = true
+        result = r
+    })
+    while (!done) {
+        yield
+    }
+    return result!
+}
+
+const fromPromise = function* <T>(promise: Promise<T>): Generator<any, T> {
+    return yield* fromClosure(done => promise.then(r => done(r)))
+}
+
+const longFive: Future<number> = function* () {
     yield* delay(1000)()
     return 5
 }
 
-const main = function* () {
+const bench = function* <T>(gen: Generator<any, T>): Generator<any, T> {
     const start = performance.now()
-
-    console.log(0)
-    const n = yield* longFive()
-    console.log(n)
-
+    const res = yield* gen
     const end = performance.now()
     console.log(`took ${end - start}ms`)
-
-    console.log(1)
-    yield* delay(1000)()
-    console.log(2)
-    yield* never()()
+    return res
 }
 
-const run = <T>(task: Task<T>) => {
+const main = function* () {
+    console.log('\nlongFive')
+    const n = yield* bench(longFive())
+    console.log(n)
+
+    console.log('\ndelay')
+    yield* bench(delay(1000)())
+
+    console.log('\nfromClosure')
+    const res = yield* bench(fromClosure(done => setTimeout(() => done('result from closure'), 1000)))
+    console.log(res)
+
+    console.log('\nfromPromise')
+    const resp = yield* bench(fromPromise(fetch('https://google.com')))
+    console.log(resp)
+
+    console.log('\nnever')
+    yield* bench(never()())
+}
+
+const run = <T>(task: Future<T>) => {
     const execution = task()
     drainAsync(execution)
 }
