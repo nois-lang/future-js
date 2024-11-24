cfa57f53b1dc9ccb7c0aba48156a4d2ee7d17712
ivanjermakov <ivanjermakov1@gmail.com>
2024-11-24 11:13:25 +0100

    Simplify future construction; `map`, `flatmap`

--- src/generators.ts
+++ src/generators.ts
@@ -1,6 +1,6 @@
 type Future<T> = { gen: Generator<unknown, T> }
 
-const future = <T, R>(f: Generator<T, R>): Future<R> => ({ gen: genMemo(f) })
+const future = <T, R>(f: () => Generator<T, R>): Future<R> => ({ gen: genMemo(f()) })
 
 type Executor = {
     futures: Future<unknown>[]
@@ -8,14 +8,16 @@ type Executor = {
 
 const executor: Executor = { futures: [] }
 
+/**
+ * Upon calling {@link Generator.next()}
+ */
 const genMemo = <T, R>(generator: Generator<T, R>): Generator<T, R> => {
-    let lastValue: T | R | undefined = undefined
+    let lastValue: T | R | undefined
     let done = false
 
     return {
-        next(...args: any): IteratorResult<T, R> {
+        next(...args: [] | [T]): IteratorResult<T, R> {
             if (done) {
-                console.log('next() on finished', lastValue)
                 return { value: lastValue as R, done: true }
             }
 
@@ -23,15 +25,20 @@ const genMemo = <T, R>(generator: Generator<T, R>): Generator<T, R> => {
             done = result.done!
             if (done && lastValue === undefined) {
                 lastValue = result.value
-                console.log('next() finished', lastValue)
-                return { done: true, value: lastValue as R }
+                return { done, value: lastValue as R }
             }
             return result
+        },
+        return(_v: R): IteratorResult<T, R> {
+            throw Error('return')
+        },
+        throw(e: any): IteratorResult<T, R> {
+            throw e
         },
         [Symbol.iterator]() {
             return this
         }
-    } as Generator<T, R>
+    }
 }
 
 const spawn = <T>(future: Future<T>, executor: Executor): Future<T> => {
@@ -76,123 +83,117 @@ const drainAsync = <T>(c: Future<T>): void => {
  * TODO: cancellation
  */
 const or = <T>(f1: Future<T>, f2: Future<T>): Future<T> =>
-    future(
-        (function* () {
-            while (true) {
-                const r1 = f1.gen.next()
-                const r2 = f2.gen.next()
-                if (r1.done) return r1.value
-                if (r2.done) return r2.value
-                yield
-            }
-        })()
-    )
+    future(function* () {
+        while (true) {
+            const r1 = f1.gen.next()
+            const r2 = f2.gen.next()
+            if (r1.done) return r1.value
+            if (r2.done) return r2.value
+            yield
+        }
+    })
+
+const map = <T, U>(fut: Future<T>, f: (value: T) => U): Future<U> =>
+    future(function* () {
+        return f(yield* fut.gen)
+    })
+
+const flatMap = <T, U>(fut: Future<T>, f: (value: T) => Future<U>): Future<U> =>
+    future(function* () {
+        return yield* f(yield* fut.gen).gen
+    })
 
 const never = () =>
-    future(
-        (function* () {
-            while (true) {
-                yield
-            }
-        })()
-    )
+    future(function* () {
+        while (true) {
+            yield
+        }
+    })
 
 const delay = (ms: number): Future<void> =>
-    future(
-        (function* () {
-            const doneBy = performance.now() + ms
-            while (doneBy >= performance.now()) {
-                yield
-            }
-        })()
-    )
+    future(function* () {
+        const doneBy = performance.now() + ms
+        while (doneBy >= performance.now()) {
+            yield
+        }
+    })
 
 const fromClosure = <T>(fn: (done: (res: T) => void) => void): Future<T> =>
-    future(
-        (function* () {
-            let done = false
-            let result: T | undefined
-            fn(r => {
-                done = true
-                result = r
-            })
-            while (!done) {
-                yield
-            }
-            return result!
-        })()
-    )
+    future(function* () {
+        let done = false
+        let result: T | undefined
+        fn(r => {
+            done = true
+            result = r
+        })
+        while (!done) {
+            yield
+        }
+        return result!
+    })
 
 const fromPromise = <T>(promise: Promise<T>): Future<T> =>
-    future(
-        (function* () {
-            return yield* fromClosure<T>(done => promise.then(r => done(r))).gen
-        })()
-    )
+    future(function* () {
+        return yield* fromClosure<T>(done => promise.then(r => done(r))).gen
+    })
 
 const longFive = (): Future<number> =>
-    future(
-        (function* () {
-            yield* delay(100).gen
-            return 5
-        })()
-    )
+    future(function* () {
+        yield* delay(100).gen
+        return 5
+    })
 
 const bench = <T>(f: Future<T>): Future<T> =>
-    future(
-        (function* () {
-            const start = performance.now()
-            const res = yield* f.gen
-            const end = performance.now()
-            console.log(`took ${end - start}ms`)
-            return res
-        })()
+    future(function* () {
+        const start = performance.now()
+        const res = yield* f.gen
+        const end = performance.now()
+        console.log(`took ${end - start}ms`)
+        return res
+    })
+
+const main = future(function* () {
+    console.log('\nlongFive')
+    const n = yield* bench(longFive()).gen
+    console.log(n)
+
+    console.log('\ndelay')
+    yield* bench(delay(100)).gen
+
+    console.log('\nfromClosure')
+    const closureRes = yield* bench(fromClosure(done => setTimeout(() => done('result from closure'), 100))).gen
+    console.log(closureRes)
+
+    console.log('\nfromPromise + map')
+    const resp = yield* bench(map(fromPromise(fetch('https://google.com')), r => r.status)).gen
+    console.log(resp)
+
+    console.log('\nfromPromise + flatMap')
+    const body = yield* bench(flatMap(fromPromise(fetch('https://google.com')), r => fromPromise(r.text()))).gen
+    console.log(body.length)
+
+    console.log('\nrace')
+    const race1 = spawn(
+        future(function* () {
+            yield* delay(50).gen
+            return 50
+        }),
+        executor
+    )
+    const race2 = spawn(
+        future(function* () {
+            yield* delay(100).gen
+            return 100
+        }),
+        executor
     )
 
-const main = future(
-    (function* () {
-        console.log('\nlongFive')
-        const n = yield* bench(longFive()).gen
-        console.log(n)
+    const res = yield* bench(or(race1, race2)).gen
+    console.log(res)
 
-        console.log('\ndelay')
-        yield* bench(delay(100)).gen
-
-        console.log('\nfromClosure')
-        const closureRes = yield* bench(fromClosure(done => setTimeout(() => done('result from closure'), 100))).gen
-        console.log(closureRes)
-
-        console.log('\nfromPromise')
-        const resp = yield* bench(fromPromise(fetch('https://google.com'))).gen
-        console.log(resp)
-
-        console.log('\nrace')
-        const race1 = spawn(
-            future(
-                (function* () {
-                    yield* delay(50).gen
-                    return 50
-                })()
-            ),
-            executor
-        )
-        const race2 = spawn(
-            future(
-                (function* () {
-                    yield* delay(100).gen
-                    return 100
-                })()
-            ),
-            executor
-        )
-
-        const res = yield* or(race1, race2).gen
-        console.log(res)
-
-        console.log('\nnever')
-        spawn(bench(never()), executor)
-    })()
-)
+    console.log('\nnever')
+    spawn(bench(never()), executor)
+})
 
 spawn(main, executor)
 run(executor)
